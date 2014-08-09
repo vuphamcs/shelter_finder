647ab6235eba40ef3d0432f6f48b0602c6c7eb98
Vu Pham <vu.pham@annkissam.com>
2014-08-09 16:25:24 -0400

    Made message smaller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,15 +13,16 @@ class TwilioController < ApplicationController
     if message_body.include?('help')
       Users.where(full: false).first(3).each do |u|
         message << "ID: #{u.id} Name: #{u.name}\n"
-        message << "Reply with an ID for more info"
       end
+
+      message << "Reply with an ID for more info"
 
     elsif message_body =~ /\A\d+\z/ ? true : false
       Users.where(id: message_body.to_i).first.each do |u|
         message << "Name: #{u.name}, Address: #{u.address}, Phone: #{u.phone}"
       end
     else
-      message << "Send 'help' to (857) 263-2905 to list shelters"
+      message << "Send 'help' to list shelters"
     end
 
     response = Twilio::TwiML::Response.new do |r|
