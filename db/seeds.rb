ada21a87656f3a3310e4e69f295f4631b155274d
Octavian Neamtu <octavian.neamtu@annkissam.com>
2014-08-09 16:40:55 -0400

    Added seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create(name: "Sojourner House", address: "85 Rockland St, Roxbury MA, 02119", phone: "(617) 442-0590", full: false)
+User.create!(name: "Sojourner House", address: "85 Rockland St, Roxbury MA, 02119", phone: "(617) 442-0590", password: 'test', password_confirmation: 'test', full: false)
 
-User.create(name: "St Francis House", address: "39 Boylston St, Boston MA, 02116", phone: "(617) 542-4211", full: false)
+User.create!(name: "St Francis House", address: "39 Boylston St, Boston MA, 02116", phone: "(617) 542-4211", password: 'test', password_confirmation: 'test', full: false)
 
-User.create(name: "Sojourner House", address: "889 Harrison Ave, Boston MA, 02118", phone: "(617) 442-9322", full: false)
+User.create!(name: "Sojourner House", address: "889 Harrison Ave, Boston MA, 02118", phone: "(617) 442-9322", password: 'test', password_confirmation: 'test', full: false)
+
+User.create!(name: "Full House", address: "889 Harrison Ave, Boston MA, 02118", phone: "(617) 442-9322", password: 'test', password_confirmation: 'test', full: true)
