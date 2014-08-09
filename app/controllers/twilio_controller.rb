79cce442aa86ccf48cae1ea04935471de015e6b7
Vu Pham <vu.pham@annkissam.com>
2014-08-09 16:09:25 -0400

    Changed messaging options

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,11 +7,22 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def messaging
-    response = Twilio::TwiML::Response.new do |r|
+    message_body = params["Body"].downcase.strip
+
+    if message_body.contains('help')
       Users.where(full: false).first(3).each do |u|
-        message << "#{u.name}, #{u.address}, #{u.phone}\n"
+        message << "ID: #{u.id} Name: #{u.name}\n"
+        message << "Reply with an ID for more info"
       end
 
+    elsif message_body =~ /\A\d+\z/ ? true : false
+      user = Users.where(id: message_body.to_i).first
+      message << "Name: #{u.name}, Address: #{u.address}, Phone: #{u.phone}"
+    else
+      message << "Send 'help' to (857) 263-2905 to list shelters"
+    end
+
+    response = Twilio::TwiML::Response.new do |r|
       r.Message message
     end
 
