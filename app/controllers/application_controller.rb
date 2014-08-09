cb9e961b28567f3f3bf5ab9d22c37270b9e94c24
Octavian Neamtu <octavian.neamtu@annkissam.com>
2014-08-09 15:44:37 -0400

    Login/logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_if_logged_in
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation) }
+  end
+
+  def redirect_if_logged_in
+    if current_user && !devise_controller?
+      redirect_to polymorphic_path([:dashboard, current_user])
+    end
   end
 end
