e92c5b89b9f2aa71bbe960f44c6863704df74e92
Vu Pham <vu.pham@annkissam.com>
2014-08-09 17:17:47 -0400

    Changed scope of active record calls

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -11,14 +11,14 @@ class TwilioController < ApplicationController
     message = ''
 
     if message_body.include?('shelter')
-      Users.where(full: false).first(3).each do |u|
+      ::Users.where(full: false).first(3).each do |u|
         message << "ID: #{u.id} Name: #{u.name}\n"
       end
 
       message << "Reply with an ID for more info"
 
     elsif message_body =~ /\A\d+\z/
-      Users.where(id: message_body.to_i).first.each do |u|
+      ::Users.where(id: message_body.to_i).first.each do |u|
         message << "Name: #{u.name}, Address: #{u.address}, Phone: #{u.phone}"
       end
     else
