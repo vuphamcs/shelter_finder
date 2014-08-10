45b04bc417d7e32aad830bfe5c08ecf9c968386e
Vu Pham <vu.pham@annkissam.com>
2014-08-10 06:12:59 -0400

    Added size to user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation, :size) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :address, :phone, :email, :password, :password_confirmation, :size) }
   end
 
   def redirect_if_logged_in
