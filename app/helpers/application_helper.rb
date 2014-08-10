7feef7806f868765aa2236f853a0111b1945d827
Vu Pham <vu.pham@annkissam.com>
2014-08-09 21:21:53 -0400

    Put in maps and some page layout stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
+require 'uri'
+
 module ApplicationHelper
   include FoundationRailsHelper::FlashHelper
+
+  def gmap(address)
+    "http://maps.googleapis.com/maps/api/staticmap?center=#{URI.escape(address)}&zoom=15&size=300x200&maptype=roadmap&markers=color:blue%7Clabel:%7C#{URI.escape(address)}"
+  end
+
+  def gmap_link(address)
+    "https://www.google.com/maps/place/#{URI.escape(address)}"
+  end
 end
