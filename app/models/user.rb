b686be8694d1a952d8c170e633f523aebcecfc38
Octavian Neamtu <octavian.neamtu@annkissam.com>
2014-08-10 12:19:21 -0400

    Smallest change.

--- app/models/user.rb
+++ app/models/user.rb
@@ -109,9 +109,9 @@ class User < ActiveRecord::Base
       message << "#{u.name}\n"
       message << "Distance: #{distance['text']} " unless distance.nil?
       message << "ID: #{u.id}\n"
-
-      message << "Reply with an ID for more info"
     end
+
+    message << "Reply with an ID for more info"
 
     message
   end
