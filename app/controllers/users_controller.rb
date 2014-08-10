f7f8bf55020e6f8ee4383a732b38438b8963b985
Octavian Neamtu <octavian.neamtu@annkissam.com>
2014-08-10 07:13:28 -0400

    Order.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(full: :asc)
   end
 
   def dashboard
